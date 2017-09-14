8b52f778e14380b7dd73cd83a8421ebce391cc46
v_changjianchi <v_changjianchi@baidu.com>
2017-09-14 18:06:35 +0800

    修改编辑脚本

--- update.js
+++ update.js
@@ -1,15 +1,7 @@
-var path = require('path');
-var fs = require('fs');
 var child_process = require('child_process');
 
-var time = path.resolve('./time.md');
-var oldTime = 0;
-var newTime = 0;
-var iTimer = null;
-
-iTimer = setInterval(function () {
-    newTime = parseInt(fs.readFileSync(time, 'utf-8'), 10);
-    if (oldTime !== newTime) {
+module.exports = function(req, res) {
+    if (req.token === 'chang') {
         child_process.exec('hexo g', {
             cwd: __dirname
         }, function (err) {
@@ -20,6 +12,8 @@ iTimer = setInterval(function () {
                 console.log('hexo g成功了');
             }
         });
-        oldTime = newTime;
     }
-}, 10000);
+    else {
+        res.end('404');
+    }
+}
